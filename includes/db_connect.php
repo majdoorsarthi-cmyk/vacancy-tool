9bce29af573dc0799c99e52215c15a247f412cb9
majdoorsarthi-cmyk <majdoorsarthi@gmail.com>
2026-09-13 08:38:10 +0530

    Update db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -2,24 +2,26 @@
 // 1. PHP में भारतीय समय (Asia/Kolkata) सेट करें
 date_default_timezone_set('Asia/Kolkata');
 
-// डेटाबेस कनेक्शन सेटिंग्स
-$servername = "100.101.113.86:3307"; 
-$username = "root";
-$password = "admin123"; 
-$dbname = "job_portal_db"; 
+// Aiven डेटाबेस कनेक्शन सेटिंग्स
+$host = "mysql-9d9cc53-majdoorsarthi-d1a8.k.aivencloud.com";
+$port = 13848;
+$username = "avnadmin";
+$password = "AVNS_CXh977fYw0GUSdyTCUU";
+$dbname = "defaultdb";
 
-// कनेक्शन बनाना
-$conn = new mysqli($servername, $username, $password, $dbname);
+// SSL कनेक्शन बनाना (Aiven के लिए आवश्यक)
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
 
-// कनेक्शन चेक करना
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// डेटाबेस से कनेक्ट करना
+if (!mysqli_real_connect($conn, $host, $username, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 
 // 2. MySQL डेटाबेस सर्वर के लिए भी टाइमज़ोन IST (+05:30) सेट करें
 $conn->query("SET time_zone = '+05:30'");
 
-// महेन्द्र जी, यह लाइन '0000-00-00' वाली डेट एरर को फिक्स कर देगी
+// '0000-00-00' वाली डेट एरर को फिक्स करने के लिए
 $conn->query("SET sql_mode=''");
 
 // हिंदी फोंट सही से दिखाने के लिए
